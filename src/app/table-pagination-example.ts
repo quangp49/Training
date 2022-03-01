eca9c8728870e1bc35e94d172fced35baed59a39
Pham Quang <qg04092001@gmail.com>
2022-03-01 13:43:14 +0700

    commit

--- src/app/table-pagination-example.ts
+++ src/app/table-pagination-example.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, LOCALE_ID } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subject } from 'rxjs';
 import { ServerHttpService } from './Services/server-http.service';
 
 /**
@@ -12,23 +13,28 @@ import { ServerHttpService } from './Services/server-http.service';
   styleUrls: ['table-pagination-example.css'],
   templateUrl: 'table-pagination-example.html',
 })
-export class TablePaginationExample implements OnInit {
+export class TablePaginationExample implements AfterViewInit {
 
   constructor(private serverHttp: ServerHttpService) { }
 
   displayedColumns: string[] = ['secCd', 'secType', 'secSName', 'secName', 'capitalValue', 'listedQty', 'foreignMaxQty', 'stockDividendRate', 'cashDividendRate', 'marketCd', 'tradingLot', 'parValue', 'maxRoom', 'status', 'remarks'];
   
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<PeriodicElement>();
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    var subject = new Subject<string>();
     this.dataSource.paginator = this.paginator;
     this.serverHttp.getProfile().subscribe((data) => {
-      console.log(data);
+      this.dataSource = new MatTableDataSource(data)
     })
+    
   }
-  
+
+  private convertsecType(){
+
+  }
 }
 
 export interface PeriodicElement {
@@ -41,4 +47,4 @@ export interface PeriodicElement {
   // listedQty: number,
 }
 
-const ELEMENT_DATA: PeriodicElement[] = []
+// const ELEMENT_DATA: PeriodicElement[] = []
