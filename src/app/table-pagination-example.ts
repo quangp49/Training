7b9a1eda52c6a127936e4e5dc56f39083aa103a0
Pham Quang <qg04092001@gmail.com>
2022-03-02 17:03:24 +0700

    commit

--- src/app/table-pagination-example.ts
+++ src/app/table-pagination-example.ts
@@ -1,4 +1,7 @@
 import { HttpClient } from '@angular/common/http';
+import { LiveAnnouncer
+ } from '@angular/cdk/a11y';
+ import {MatSort, Sort} from '@angular/material/sort';
 import { AfterViewInit, Component, OnInit, ViewChild, LOCALE_ID } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -15,25 +18,35 @@ import { ServerHttpService } from './Services/server-http.service';
 })
 export class TablePaginationExample implements AfterViewInit {
 
-  constructor(private serverHttp: ServerHttpService) { }
+  constructor(private serverHttp: ServerHttpService, private _liveAnnouncer: LiveAnnouncer) { }
 
   displayedColumns: string[] = ['secCd', 'secType', 'secSName', 'secName', 'capitalValue', 'listedQty', 'foreignMaxQty', 'stockDividendRate', 'cashDividendRate', 'marketCd', 'tradingLot', 'parValue', 'maxRoom', 'status', 'remarks'];
   
   dataSource = new MatTableDataSource<PeriodicElement>();
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngAfterViewInit(): void {
     var subject = new Subject<string>();
-    this.dataSource.paginator = this.paginator;
     this.serverHttp.getProfile().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data)
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
     
   }
-
-  private convertsecType(){
-
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
 
