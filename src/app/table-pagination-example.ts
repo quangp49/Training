ce8e9adc23229be1013ccaffb70e144c636ad312
quangp49 <65329128+quangp49@users.noreply.github.com>
2022-03-07 14:04:34 +0700

    search

--- src/app/table-pagination-example.ts
+++ src/app/table-pagination-example.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { LiveAnnouncer
- } from '@angular/cdk/a11y';
- import {MatSort, Sort} from '@angular/material/sort';
+import {
+  LiveAnnouncer
+} from '@angular/cdk/a11y';
+import { MatSort, Sort } from '@angular/material/sort';
 import { AfterViewInit, Component, OnInit, ViewChild, LOCALE_ID } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subject } from 'rxjs';
+
 import { ServerHttpService } from './Services/server-http.service';
 
 /**
@@ -20,8 +22,9 @@ export class TablePaginationExample implements AfterViewInit {
 
   constructor(private serverHttp: ServerHttpService, private _liveAnnouncer: LiveAnnouncer) { }
 
+  public searchInput: string;
   displayedColumns: string[] = ['secCd', 'secType', 'secSName', 'secName', 'capitalValue', 'listedQty', 'foreignMaxQty', 'stockDividendRate', 'cashDividendRate', 'marketCd', 'tradingLot', 'parValue', 'maxRoom', 'status', 'remarks'];
-  
+
   dataSource = new MatTableDataSource<PeriodicElement>();
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -33,26 +36,25 @@ export class TablePaginationExample implements AfterViewInit {
       this.dataSource = new MatTableDataSource(data)
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
+      //Search
+
     })
-    
   }
-  /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
       this._liveAnnouncer.announce('Sorting cleared');
     }
   }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
 
 export interface PeriodicElement {
-  // action: string,
-  // secCd: string,
+  secCd: string,
   // secType: string,
   // secSName: string,
   // secName: string,
