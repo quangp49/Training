3b1dc04a8f256967fdcad33f8234713acbce625a
quangp49 <65329128+quangp49@users.noreply.github.com>
2022-03-07 09:27:43 +0700

    enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 
-import {MaterialExampleModule} from '../material.module';
-import {TablePaginationExample} from './table-pagination-example';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {HttpClientModule} from '@angular/common/http';
+import { MaterialExampleModule } from '../material.module';
+import { TablePaginationExample } from './table-pagination-example';
+import { CustomPaginator } from './Services/CustomPaginator';
 
 @NgModule({
   declarations: [TablePaginationExample],
@@ -19,7 +21,7 @@ import {HttpClientModule} from '@angular/common/http';
     MaterialExampleModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [{ provide: MatPaginatorIntl, useValue: CustomPaginator() }],
   bootstrap: [TablePaginationExample],
 })
-export class AppModule {} 
+export class AppModule { } 
